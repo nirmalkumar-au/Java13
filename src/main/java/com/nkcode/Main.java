a31cc107af5610758cb300519fcba043a6978261
nirmalkumar-au <nirmalkumar-au@users.noreply.github.com>
2022-09-29 15:21:50 +1000

    Unsigned wrapper exercise

--- src/main/java/com/nkcode/Main.java
+++ src/main/java/com/nkcode/Main.java
@@ -2,6 +2,11 @@ package com.nkcode;
 
 public class Main {
     public static void main(String[] args) {
+        checkPrimitiveWrapper();
+        checkUnsignedWrapper();
+    }
+
+    private static void checkPrimitiveWrapper() {
         String doubleValue = "156.99d";
         Double doubleObj = Double.parseDouble(doubleValue);
         var intValue = doubleObj.intValue();
@@ -15,5 +20,17 @@ public class Main {
         System.out.println("byteValue " + byteValue);
         System.out.println("stringValue " + stringValue);
         System.out.println("storedValue " + storedValue);
+    }
+
+    private static void checkUnsignedWrapper() {
+        String intValue = "300000000";
+        Integer intUnsignedObj = Integer.parseUnsignedInt(intValue);
+        System.out.println("Unsigned value " + intUnsignedObj);
+
+        var result = Integer.divideUnsigned(intUnsignedObj, 7);
+        System.out.println("Divide unsigned value " + result);
+
+        result = Integer.remainderUnsigned(intUnsignedObj, 7);
+        System.out.println("Remainder unsigned value " + result);
     }
 }
